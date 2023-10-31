07daffd16bdc31e411fdfff08ac7869c2e03f37d
DeshiHacker <manis.programmer@gmail.com>
2023-10-31 16:27:23 -0400

    Add files via upload

---
+++ CV_Maker.py
@@ -0,0 +1,24 @@
+name = input("what's your name: ")
+age = input("enter your age here: ")
+last_education = input("type your last education degree here: ")
+zila = input("type your zila name: ")
+contact = input("please type your phone number: ")
+contact2 = input("enter your email:  ")
+
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print("____________________")
+
+print(f'''I am {name}. I am {age} years old.
+I have completed {last_education}.
+{zila} is my home district.
+Here is my contact number:phone{contact}.
+email: {contact2}. ''')
+
+
+
+
+
